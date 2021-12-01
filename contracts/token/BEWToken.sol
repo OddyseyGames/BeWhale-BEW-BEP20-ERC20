ba1c7178f0e2a8a08fc8e9b9dbb9a9e8b5076adc
Patrick Oddysey <accounts@oddysey.games>
2021-12-01 18:35:21 +0000

    Switch to BSC network

--- contracts/token/BEWToken.sol
+++ contracts/token/BEWToken.sol
@@ -16,25 +16,25 @@ contract BEWToken is ERC20Extended, Pausable, AccessControl {
     uint256 private dFactor = 10 ** uint256(18);
 
     constructor() ERC20("Be Whale Token", "BEW", 18) {
-        // Binance Smart Chain network
-        //_setupSupply(30000000 * dFactor); // id:0; Founders [via Vesting]
-        //_setupSupply(15000000 * dFactor); // id:1; Advisors
-        //_setupSupply(90000000 * dFactor); // id:2; Public Sale via https://bew.dev
-        //_setupSupply(7500000 * dFactor);  // id:3; Affiliate
-        //_setupSupply(7500000 * dFactor);  // id:4; Airdrop
-        //_setupSupply(90000000 * dFactor); // id:5; Staking
-        //_setupSupply(60000000 * dFactor); // id:6; Whale Bridge
+         // Binance Smart Chain network
+        _setupSupply(30000000 * dFactor); // id:0; Founders [via Vesting]
+        _setupSupply(15000000 * dFactor); // id:1; Advisors
+        _setupSupply(90000000 * dFactor); // id:2; Public Sale via https://bew.dev
+        _setupSupply(7500000 * dFactor);  // id:3; Affiliate
+        _setupSupply(7500000 * dFactor);  // id:4; Airdrop
+        _setupSupply(90000000 * dFactor); // id:5; Staking
+        _setupSupply(60000000 * dFactor); // id:6; Whale Bridge
 
-         // Init supply for Founders | 4.5 millions first allocation
-        //_mint(msg.sender, 4500000 * dFactor, 0);
+        // Init supply for Founders | 4.5 millions first allocation
+        _mint(msg.sender, 4500000 * dFactor, 0);
 
         // Ethereum network
-        _setupSupply(50000000 * dFactor); // id:0; Rewards for completing game quests
-        _setupSupply(20000000 * dFactor); // id:1; DAO rewards
-        _setupSupply(40000000 * dFactor); // id:2; Staking
-        _setupSupply(30000000 * dFactor); // id:3; Marketing
-        _setupSupply(20000000 * dFactor); // id:4; Strategic reserve
-        _setupSupply(40000000 * dFactor); // id:5; Whale Bridge
+        //_setupSupply(50000000 * dFactor); // id:0; Rewards for completing game quests
+        //_setupSupply(20000000 * dFactor); // id:1; DAO rewards
+        //_setupSupply(40000000 * dFactor); // id:2; Staking
+        //_setupSupply(30000000 * dFactor); // id:3; Marketing
+        //_setupSupply(20000000 * dFactor); // id:4; Strategic reserve
+        //_setupSupply(40000000 * dFactor); // id:5; Whale Bridge
 
         _setupRole(SUPER_ADMIN_ROLE, msg.sender);
         _setupRole(BURNER_ROLE, msg.sender);
